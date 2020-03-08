c0ba025943cc323bae3c04ff23d1817ee43b60fc
najmaA <najmaamin10@gmai.com>
2020-03-08 06:40:31 +0300

    configuration

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http  import HttpResponse,Http404
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
