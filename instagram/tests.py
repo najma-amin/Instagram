12fc57a68f1ed6dcfe1072de84734afcbccc1673
najmaA <najmaamin10@gmai.com>
2020-03-08 07:00:50 +0300

    instance test added

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Profile, Post
+
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile = Profile(user='Amjaan', name = 'Amjaan', profile_pic = 'home.png')
+        self.profile.save()
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post = Post(image = 'home.png', caption = "tumaa' post")
+        self.post.save()
+
+    def tearDown(self):
+        Post.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
