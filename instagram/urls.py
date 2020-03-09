0be5dc0c1dc5fcc22ff27689fd219938002994fb
najmaA <najmaamin10@gmai.com>
2020-03-09 15:38:03 +0300

    Correct url link

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'', views.index, name='index'),
     url(r'^accounts/profile/$', views.profile, name='profile'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^new/post$', views.new_post, name='new_post'),
