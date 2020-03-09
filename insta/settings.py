4c490b216a50e1e873da31b1267eda3c40bcf45c
najmaA <najmaamin10@gmai.com>
2020-03-09 14:49:14 +0300

    indent fix

--- insta/settings.py
+++ insta/settings.py
@@ -19,15 +19,13 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
-if config('MODE')=="dev":
-   DATABASES = {
+        DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
+           'NAME': 'instagram',
+           'USER': 'user',
+           'PASSWORD': 'najma6498',
+           
        }
        
    }
